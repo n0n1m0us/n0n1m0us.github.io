002be72e842a38f58e5867d2d3b0374aa512ef3b
n0n1m0us <81222456+n0n1m0us@users.noreply.github.com>
2021-08-24 18:14:35 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -7,8 +7,8 @@ $(document).ready(() => {
 });
 
 var typed = new Typed('#typed', {
-  strings: ['<p class="header1">n0n1m0us</p>^1000\n `<p class="header3">Student / Pentester / CTF Player</p>` ^400\n `<a class="link1" href="https://twitter.com/n0n1m0us" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fab fa-twitter"></i></a>` , `<a class="link1" href="https://github.com/n0n1m0us" data-toggle="tooltip" data-placement="bottom" title="Github"><i class="fab fa-github"></i></a>`'],
-  typeSpeed: 80,
+  strings: ['<p class="header1">n0n1m0us</p>^400\n `<p class="header3">Student / Pentester / CTF Player</p>` ^400\n `<a class="link1" href="https://twitter.com/n0n1m0us" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fab fa-twitter"></i></a>` , `<a class="link1" href="https://github.com/n0n1m0us" data-toggle="tooltip" data-placement="bottom" title="Github"><i class="fab fa-github"></i></a>`'],
+  typeSpeed: 90,
   backSpeed: 0,
   loop: false,
   cursorChar: '|',
