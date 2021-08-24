9b17b0d5eb9fe7d6a90c19bba58afdae7b615b95
n0n1m0us <81222456+n0n1m0us@users.noreply.github.com>
2021-08-24 16:17:56 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,12 @@ $(document).ready(() => {
 
   particlesJS.load('particles-js', 'particlesjs.json');
 });
+
+var typed = new Typed('#typed', {
+  strings: ['<p class="header1">n0n1m0us</p>^1000\n `<p class="header3">Student / Pentester / CTF Player</p>` ^400\n `<a class="link1" href="https://twitter.com/n0n1m0us" data-toggle="tooltip" data-placement="bottom" title="Twitter"><i class="fab fa-twitter"></i></a>` , `<a class="link1" href="https://github.com/n0n1m0us" data-toggle="tooltip" data-placement="bottom" title="Github"><i class="fab fa-github"></i></a>`'],
+  typeSpeed: 80,
+  backSpeed: 0,
+  loop: false,
+  cursorChar: '|',
+  fadeout: true,
+});
